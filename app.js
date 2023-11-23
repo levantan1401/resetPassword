9a5e535df0f0c8cf4ff11e3d33456b3b29bc3806
levantan1401 <levantan1401.developer@gmail.com>
2023-11-23 09:43:41 +0700

    set Endpoint and new key

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const sdk = new window.Appwrite();
-sdk.setEndpoint("https://cloud.appwrite.io/v1").setProject("653b9e4568854c39355d");
+sdk.setEndpoint("https://reset-password-alpha.vercel.app/v1").setProject("653b9e4568854c39355d");
 
 const urlParams = new URLSearchParams(window.location.search);
 const userId = urlParams.get("userId");
 const secret = urlParams.get("secret");
 
-const promise_forgotpassword = account.createRecovery('email@example.com', '');
+// const promise_forgotpassword = account.createRecovery('email@example.com', '');
 
-promise.then(function (response) {
-    console.log(response);
-}, function (error) {
-    console.log(error);
-});
+// promise.then(function (response) {
+//     console.log(response);
+// }, function (error) {
+//     console.log(error);
+// });
 
 let promise = sdk.account.updateVerification(userId, secret);
 promise.then(function (res){
     console.log(res);
     document.getElementById("message").innerText = JSON.stringify(res);
-    window.location.replace('appwrite-callback-653c7e3f9d64ecd21392://index');
+    window.location.replace('appwrite-callback-653b9e4568854c39355d://index');
 }, function(error) {
     console.log(error);
     document.getElementById("message").innerText = JSON.stringify(error);
